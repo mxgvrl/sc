bf4398b47f8ff0441756a8b9f28596c33482230a
mxgvrl <mxgvrl@gmail.com>
2020-11-15 02:41:09 +0300

    table out + algorithm

--- Sieve/index.js
+++ Sieve/index.js
@@ -45,34 +45,87 @@ function start1(x){
     }
 }
 
-function start2(){ //запускается при клике на кнопку
+function start2(){
     let s = "<table border=\"1\" cellpadding=\"5\">";
     const userCount = document.getElementById("users").value;
     const x = parseInt(userCount);
+    filling(x);
     let y = Math.sqrt(x);
     y = y.toFixed();
     if (y*y < x) {
-        console.log(+y+1 + 'x' + y);
+        let adding = -y + 1;
+        // let adding = 0;
         for (let j = 0; j < +y; j++) {
             s += "<tr>";
+            adding += +y + 1;
             for(let i = 0; i < +y+1; i++){
-                s += "<td>" + i + "</td>";
+                if ((+i + +adding) <= x){
+                    s += "<td>" + (+i + +adding) + "</td>";
+                }
             }
             s += "</tr>";
         }
     }
     else {
-        console.log(y + 'x' + y);
+        for (let j = 0; j < +y; j++) {
+            s += "<tr>";
+            for(let i = 0; i < +y; i++){
+                s += "<td>" + sieve[i] + "</td>";
+            }
+            s += "</tr>";
+        }
     }
-    // for(let i = 0; i < x; i++){
-    //     s += "<tr><td>" + i + "</td></tr>";       //новая строка таблицы для каждого пользователя
-    // }
     s += "</table>";
 
-    document.body.innerHTML += s; //добавил в body, на самом деле можно добавить куда хотите))
+    document.body.innerHTML += s;
 }
 
-start1(4);
+function start3(){
+    let s = "<table border=\"1\" cellpadding=\"5\">";
+    const userCount = document.getElementById("users").value;
+    const x = parseInt(userCount);
+    filling(x);
+    let y = Math.sqrt(x);
+    y = +y.toFixed();
+    if (y*y < x) {
+        s += addToTable(x, y,1, s)
+    }
+    else {
+        s += addToTable(x, y,0, s)
+    }
+    s += "</table>";
+    document.body.innerHTML += s;
+}
+
+function addToTable(square ,minSide, isNotSquare, tableString) {
+    let adder = 0;
+    for (let i = 0; i < minSide+isNotSquare; i++) {
+        tableString += '<tr>'
+        i > 0 ? adder += minSide : adder;
+        for (let j = 2; j < minSide+2; j++) {
+            if ((j + adder) <= square){
+                i > 0 ? tableString += '<td>' + (+j + +adder) : tableString += '<td>' + (+j);
+            }
+        }
+        tableString += '</tr>'
+    }
+    return tableString;
+}
+
+function sieveTraverse1 () {
+    const userCount = document.getElementById("users").value;
+    const n = parseInt(userCount);
+    let bar = document.getElementsByTagName('td');
+    let coo = Array.prototype.slice.call(bar);
+        for (let i = 0; (i*i) <= n; i++) {
+            if (sieve[i] === true) {
+                for (let k = 0, j = i*i; j < n+1; ++k, j = (i*i) + (k*i)) {
+                    coo[j-2].innerHTML = '<tb style="background:#ff4141;">'+ +j +'</tb>'; // todo: animate
+                    sieve[j] = false;
+                }
+            }
+        }
+}
 
 // filling(50);
 // output();
