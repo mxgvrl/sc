10db4b5ab92a2be4a57bb1114a17cd961af2a111
mxgvrl <mxgvrl@gmail.com>
2020-11-15 15:59:51 +0300

    added to original project

--- Sieve/index.js
+++ Sieve/index.js
@@ -22,66 +22,8 @@ function sieveTraverse (n) {
      }
 }
 
-function start(){ //запускается при клике на кнопку
-    let s = "<table border=\"1\" cellpadding=\"5\">";
-    const userCount = document.getElementById("users").value;    //получил значение из input
-    const x = parseInt(userCount); //преобразовал из строки в число
-    for(let i = 0; i < x; i++){
-        s += "<tr><td>" + i + "</td></tr>";       //новая строка таблицы для каждого пользователя
-    }
-    s += "</table>";
-
-    document.body.innerHTML += s; //добавил в body, на самом деле можно добавить куда хотите))
-}
-
-function start1(x){
-    let y = Math.sqrt(x);
-    y = y.toFixed();
-    if (y*y < x) {
-        console.log(+y+1 + 'x' + y);
-    }
-    else {
-        console.log(y + 'x' + y);
-    }
-}
-
-function start2(){
-    let s = "<table border=\"1\" cellpadding=\"5\">";
-    const userCount = document.getElementById("users").value;
-    const x = parseInt(userCount);
-    filling(x);
-    let y = Math.sqrt(x);
-    y = y.toFixed();
-    if (y*y < x) {
-        let adding = -y + 1;
-        // let adding = 0;
-        for (let j = 0; j < +y; j++) {
-            s += "<tr>";
-            adding += +y + 1;
-            for(let i = 0; i < +y+1; i++){
-                if ((+i + +adding) <= x){
-                    s += "<td>" + (+i + +adding) + "</td>";
-                }
-            }
-            s += "</tr>";
-        }
-    }
-    else {
-        for (let j = 0; j < +y; j++) {
-            s += "<tr>";
-            for(let i = 0; i < +y; i++){
-                s += "<td>" + sieve[i] + "</td>";
-            }
-            s += "</tr>";
-        }
-    }
-    s += "</table>";
-
-    document.body.innerHTML += s;
-}
-
 function start3(){
-    let s = "<table border=\"1\" cellpadding=\"5\">";
+    let s = "<table border=\"1\" cellpadding=\"5\" class='tab-content'>";
     const userCount = document.getElementById("users").value;
     const x = parseInt(userCount);
     filling(x);
@@ -94,7 +36,7 @@ function start3(){
         s += addToTable(x, y,0, s)
     }
     s += "</table>";
-    document.body.innerHTML += s;
+    document.getElementById('div').innerHTML += s;
 }
 
 function addToTable(square ,minSide, isNotSquare, tableString) {
@@ -112,26 +54,10 @@ function addToTable(square ,minSide, isNotSquare, tableString) {
     return tableString;
 }
 
-function sieveTraverse1 () {
-    const userCount = document.getElementById("users").value;
-    const n = parseInt(userCount);
-    let bar = document.getElementsByTagName('td');
-    let coo = Array.prototype.slice.call(bar);
-
-    for (let i = 0; (i*i) <= n; i++) {
-        if (sieve[i] === true) {
-            for (let k = 0, j = i*i; j < n+1; ++k, j = (i*i) + (k*i)) {
-                coo[j-2].innerHTML = '<tb style="background:#ff4141;">'+ +j +'</tb>';
-                sieve[j] = false ;
-            }
-        }
-    }
-}
-
 function sieveTraverse2 () {
     const userCount = document.getElementById("users").value;
     const n = parseInt(userCount);
-    let bar = document.getElementsByTagName('td');
+    let bar =  document.getElementById('div').getElementsByTagName('td');
     let coo = Array.prototype.slice.call(bar);
     let i = 0;
     let j = i*i;
