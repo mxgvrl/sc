0a1ccc6ea9b3c3d8dd2efa485c0d105dbe342a3c
mxgvrl <mxgvrl@gmail.com>
2020-12-06 13:48:29 +0300

    tasks are done

--- LinkedList/LinkedList/Node.cs
+++ LinkedList/LinkedList/Node.cs
@@ -1,16 +1,17 @@
-﻿namespace LinkedList {
+﻿using System;
+
+namespace LinkedList {
     public class Node {
         public int value;
         public Node next;
         public Node prev;
         public int index;
+        public Node root;
         public override string ToString() {
             return $"[{this.index}] {this.value}";
         }
 
-        public Node() {
-            
-        }
+        public Node() { }
         
         public Node(Node previous, int index, int value) {
             this.index = index;
@@ -18,6 +19,9 @@
             if (previous != null) {
                 this.prev = previous;
                 previous.next = this;
+            }
+            else {
+                this.root = this;
             }
         }
         
@@ -44,6 +48,103 @@
                     this.index = index;
                 }
                 tempNode = tempNode.prev;
+            }
+        }
+
+        private static Node FindNodeByIndex(Node node, int index) {
+            if(node.index == index) {
+                return node;
+            }
+            if(node.next != null) {
+                return FindNodeByIndex(node.next, index);
+            }
+            Console.WriteLine($"Node with index {index} not found");
+            return null;
+        }
+        
+        public void ReadByIndex(int index) {
+            Node node = FindNodeByIndex(root, index);
+            if (node != null) {
+                Console.WriteLine($"Node: {node}");
+            }
+        }
+        
+        public void RemoveByIndex(int index)
+        {
+            var marked = FindNodeByIndex(root, index);
+            if (marked == null) return;
+            if(marked.prev != null && marked.next != null)
+            {
+                marked.prev.next = marked.next;
+                marked.next.prev = marked.prev;
+            } 
+            else
+            {
+                if (marked.next != null) {
+                    marked.next.prev = null;
+                    root = marked.next;
+                }
+                else {
+                    marked.prev.next = null;
+                }
+            }
+        }
+
+        private void OutList(Node node) {
+            Console.Write($"{node}  ");
+            if (node.next != null) {
+                OutList(node.next);
+            }
+        }
+
+        public void OutLinkedList() {
+            OutList(root);
+            Console.WriteLine();
+        }
+
+        void swap(Node node, Node node2) {
+            if (node.prev != null && node2.next != null) {
+                node.next = node2.next;
+                node.next.prev = node;
+                node2.prev = node.prev;
+                node2.prev.next = node2;
+            }
+
+            if (node2.next == null) {
+                node.next = null;
+                node2.prev = node.prev;
+                node2.prev.next = node2;
+            }
+            
+            if (node.prev == null) {
+                node.next = node2.next;
+                node.next.prev = node;
+                node2.prev = null;
+                node2.prev.next = node2;
+            }
+
+            if (node.prev == null && node2.next == null) {
+                node.next = null;
+                node2.prev = null;
+            }
+            
+            node2.next = node;
+            node.prev = node2;
+        }
+        
+        public void Sort()
+        {
+            var temp = root;
+            while (temp.next != null)
+            {
+                if (temp.index > temp.next.index)
+                {
+                    swap(temp, temp.next);
+                    if (temp.next != null) temp = temp.next;
+                    Sort();
+                }
+                else
+                    temp = temp.next; 
             }
         }
     }
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 namespace LinkedList {
     internal class Program {
         public static void Main(string[] args) {
-            var testList = new LinkedList();
             var node = new Node(null, 0, 1);
             var node2 = new Node(node, 1, 2);
             var node3 = new Node(node2, 4, 3);
@@ -16,7 +15,15 @@ namespace LinkedList {
             // Console.WriteLine(node2.prev);
 
             var node6 = new Node(node4, 3);
-            Console.WriteLine(node6);
+            //Console.WriteLine(node6);
+            
+            node.ReadByIndex(0);
+            node.OutLinkedList();
+            node.RemoveByIndex(3);
+            node.OutLinkedList();
+
+            node.Sort();
+            node.OutLinkedList();
         }
     }
 }
