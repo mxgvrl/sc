06df1f78c67da6227fd107948b216c075fb170aa
mxgvrl <mxgvrl@gmail.com>
2020-12-13 20:54:56 +0300

    linq

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ {
+    internal class Program {
+        public static void Main(string[] args) {
+            task1();
+            task2();
+        }
+
+        static void task1() {
+            int[] numbers = { 33, -2, 3, 1, 10, -34, 55, -66, 77, 88 };
+            var t1 = numbers.Where(i => i > 0).Min();
+            Console.WriteLine(t1);
+        }
+
+        static void task2() {
+            string[] strings = { "AKDSFJ", "HDFAADGREDF", "ASGQERGSDFB", "PLSA", "ZEKNRG", "OAAO"};
+            var t2 = strings.OrderBy(s => s.Length).ThenBy(s => s);
+            foreach (var n in t2) {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
--- VernamCipher/VernamCipher/Program.cs
+++ VernamCipher/VernamCipher/Program.cs
@@ -8,30 +8,11 @@ using System.Numerics;
 namespace VernamCipher {
     internal class Program {
         public static void Main(string[] args) {
-            // Example1();
-            // Console.WriteLine();
-            // Example2();
-            // Console.WriteLine(Eq(100));
-            
+
             StringBuilder key0 = new StringBuilder("1001001");
             string key = key0.ToString();
-            // int keyLength = key.Length;
-            //
+
             String str = "alma-mater";
-            //
-            // StringBuilder res = new StringBuilder();
-            // string preBinary = Convert.ToString('t', 2);
-            //
-            // for (int i = 0; i < preBinary.Length - keyLength; i++) {
-            //     key.Append((char)key[i]);
-            // }
-            //
-            // int binary = Convert.ToInt32(preBinary, 2);
-            // //var bar1 = Convert.ToInt32(binary.ToString(), 2);
-            // var key1 = Convert.ToInt32(key.ToString(), 2);
-            // res.Append((char) (binary ^ key1));
-            //
-            // Convert.ToInt32("1001101", 2).ToString();
             
             String s1  = vernam(str, key);
             String s2  = vernam(s1, key);
@@ -57,9 +38,6 @@ namespace VernamCipher {
                 strArray[iterator] += (Convert.ToString(c, 2));
                 iterator++;
             }
-            
-            //string[] a = charArray.Select(c => Convert.ToString(c, 2));
-
             int symbCode = 0;
             int keyCode = 0;
             for (int i = 0; i < strArray.Length - 1; i++) {
@@ -70,17 +48,6 @@ namespace VernamCipher {
                 keyCode = Convert.ToInt32(key.ToString(), 2);
                 res.Append((char) (symbCode ^ keyCode));
             }
-
-            
-            
-            // StringBuilder res = new StringBuilder();
-            // int binary = 0;
-            // foreach (char c in s.ToCharArray()) {
-            //     binary = Convert.ToInt32(Convert.ToString(c, 2));
-            //     //res.Append((char) (binary ^ key));
-            //     res.Append((char) (Convert.ToInt32(binary.ToString(), 2)  ^ Convert.ToInt32(key.ToString(), 2)));
-            //
-            // }
             return res.ToString();
         }
     }
